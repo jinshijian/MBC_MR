db60236eb4609ce0b15f813a9fc14a62adcb266a
jinshi <jianjinshi734@gmail.com>
2023-03-21 09:01:11 +0800

    resample code update
    resample code update

--- code/main_analysis_1_resample_and_plot_v2.R
+++ code/main_analysis_1_resample_and_plot_v2.R
@@ -1,3 +1,4 @@
+
 library(raster)
 library(tools)
 library(sf)
@@ -18,7 +19,9 @@ library(gt)
 # library(glcpck)
 source(here::here("code/functions.R"))
 
-# main dataset -------------------------------------------------
+# ******************************************************************************
+# load dataset 
+# ******************************************************************************
 cmic0 <- glc_proc_data() %>% mutate(tmean = tmean/10-273)
 cmic1 <- read.csv("rawdata/mbc_meta_final2.csv") %>% 
   mutate(land_cover = factor(land_cover, levels = c("C", "FB", "FC", "FT", "G", "S"))) %>% 
@@ -35,49 +38,52 @@ cmic <- as.data.frame(cmic2) # needed
 lm(Cmic ~ tmean, data = cmic0) %>% summary()
 lm(Cmic ~ tmean, data = cmic1) %>% summary()
 
-# **********************************************************************
-# ---------- random sample and building linear model ---------
-# scenario 1: rasample (n=110) 60 times with slope of lm from
-# the subsamples within seq(-0, -12, -2)
-# **********************************************************************
-sample_cmic_lm = function(sdata){
-  sdata[sample(nrow(sdata), 110), ] -> sub_cmic # sample size =110
+summary(cmic)
+
+## create function for resample ---------------------------
+sample_cmic0_lm = function(sdata){
+  sdata[sample(nrow(sdata), 500, replace=TRUE), ] -> sub_cmic 
   lm_cmic = lm(Cmic ~ tmean, data = sub_cmic) %>% summary()
   lm_cmic$coefficients[2,1] -> lm_slope
   sub_cmic$Slope = lm_slope
   return(sub_cmic)
 }
 
-check_point = seq(-0, -12, -2)
+# ******************************************************************************
+# ---------- random sample and building linear model ---------
+# --scenario 1: randomly resample 100 times (n=500) from Patoine (n=762) 
+# ******************************************************************************
+cmic0 <- glc_proc_data() %>% mutate(tmean = tmean/10-273)
+predictors <- c("Cmic","latitude","longitude", glc_layers()) %>% unname
+lm(Cmic ~ tmean, data = cmic0) %>% summary()
+
+predictors <- c("Cmic","latitude","longitude", glc_layers()) %>% unname
+cmic2 <- cmic0[,predictors]  
+cmic <- as.data.frame(cmic2) # needed
+
+## resample 200 times -------------------------------------
 outputs <- cmic2[0,]
 
-set.seed(20220926)
-for(i in 1:6){
-  scenario = check_point[6] # i from 1:6
-  for(j in 1:100000) {
-    sample_cmic_lm(cmic) -> sub_cmic
-    if (abs(sub_cmic$Slope[1] - scenario) < 1 ) {
-      outputs <- bind_rows(outputs, sub_cmic)
-      print(paste0('loop','****',j))
-      break
-    } 
-    else {next}
-    # return(outputs)
-  }
-  print(paste0('scenario','****',i))
+set.seed(123456)
+
+for(i in 1:100){
+  sample_cmic0_lm(cmic) -> sub_cmic
+  run_number <- tibble(run_number = rep(i, 500))
+  sub_cmic <- bind_cols(sub_cmic, run_number)
+  outputs <- bind_rows(outputs, sub_cmic)
+  print(paste0("----------",i))
 }
-outputs$Slope %>% unique() %>% sort()
-# write.csv(outputs,"output/resampled_cmic.csv", row.names = F)
 
-outputs = read.csv("output/resampled_cmic.csv") %>% 
-  mutate(land_cover = factor(land_cover, levels = c("C", "FB", "FC", "FT", "G", "S")))
+summary(outputs)
+
+write.csv(outputs,"output/resampled_cmic_500_patoine.csv", row.names = F)
 
 
-# **********************************************************************
+# ******************************************************************************
 # ---------- random sample and building linear model ---------
 # --scenario 2: randomly resample 100 times (n=500) from combined
 # data of Patoine (n=762) and control of warming experiment (n=106)
-# **********************************************************************
+# ******************************************************************************
 cmic0 <- glc_proc_data() %>% mutate(tmean = tmean/10-273)
 cmic1 <- read.csv("rawdata/mbc_meta_final2.csv") %>% 
   mutate(land_cover = factor(land_cover, levels = c("C", "FB", "FC", "FT", "G", "S"))) %>% 
@@ -90,57 +96,28 @@ cmic2 <- bind_rows(
   cmic0[,predictors],
   cmic1[,predictors])  
 cmic <- as.data.frame(cmic2) # needed
+cmic %>% summary()
 
-sample_cmic0_lm = function(sdata){
-  sdata[sample(nrow(sdata), 500), ] -> sub_cmic 
-  lm_cmic = lm(Cmic ~ tmean, data = sub_cmic) %>% summary()
-  lm_cmic$coefficients[2,1] -> lm_slope
-  sub_cmic$Slope = lm_slope
-  return(sub_cmic)
-}
+# test lm between MAT and MBC
+lm(cmic0$Cmic ~ cmic0$tmean) %>% summary()
+lm(cmic1$Cmic ~ cmic1$tmean) %>% summary()
+lm(cmic$Cmic ~ cmic$tmean) %>% summary()
 
+
+## resample 200 times ---------------------------------------------------------
 outputs <- cmic2[0,]
 
-set.seed(20220926)
-for(i in 1:100){
+# set.seed(123456)
+for(i in 1:200){
   sample_cmic0_lm(cmic) -> sub_cmic
+  run_number <- tibble(run_number = rep(i, 500))
+  sub_cmic <- bind_cols(sub_cmic, run_number)
   outputs <- bind_rows(outputs, sub_cmic)
   print(paste0("----------",i))
 }
 
-write.csv(outputs,"output/resampled_cmic_500_combined.csv", row.names = F)
-
-
-# **********************************************************************
-# ---------- random sample and building linear model ---------
-# --scenario 3: randomly resample 100 times (n=500) from Patoine (n=762) 
-# **********************************************************************
-cmic0 <- glc_proc_data() %>% mutate(tmean = tmean/10-273)
-predictors <- c("Cmic","latitude","longitude", glc_layers()) %>% unname
-lm(Cmic ~ tmean, data = cmic0) %>% summary()
-
-predictors <- c("Cmic","latitude","longitude", glc_layers()) %>% unname
-cmic2 <- cmic0[,predictors]  
-cmic <- as.data.frame(cmic2) # needed
-
-sample_cmic0_lm = function(sdata){
-  sdata[sample(nrow(sdata), 500), ] -> sub_cmic 
-  lm_cmic = lm(Cmic ~ tmean, data = sub_cmic) %>% summary()
-  lm_cmic$coefficients[2,1] -> lm_slope
-  sub_cmic$Slope = lm_slope
-  return(sub_cmic)
-}
-
-outputs <- cmic2[0,]
-
-set.seed(20220926)
-for(i in 1:100){
-  sample_cmic0_lm(cmic) -> sub_cmic
-  outputs <- bind_rows(outputs, sub_cmic)
-  print(paste0("----------",i))
-}
-
-write.csv(outputs,"output/resampled_cmic_500_patoine.csv", row.names = F)
+summary(outputs)
+# write.csv(outputs,"output/resampled_cmic_500_combined.csv", row.names = F)
 
 
 
